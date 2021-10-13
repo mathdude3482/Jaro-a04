c5c50e2e28790a5944e2dbc2fb2d149bafde68f1
mathdude3482 <orajsamej@gmail.com>
2021-10-12 21:53:58 -0400

    Initial idea of the solution along with a UML diagram.

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,25 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 James Jaro
+ */
+//Construct a program that reads in the following data file
+//and processes the records and display the results
+// formatted as a table, evenly spaced, as shown in the example output.
+public class Solution42 {
+    public static void main(String[] args) {
+        //take in a file called exercise42_input and call a method called setInfo to read it.
+        //call a method called setData to store the data into their respective lists.
+        //call a method called display to display the data in a tabular format.
+    }
+    public void setInfo(){
+        //read in information from the file and store them into an arrayList called info.
+    }
+    public void setData(){
+        //using a for each loop, split the arrayList by commas and store their respective values into
+        //seperate array lists.
+    }
+    public void display(){
+        //display the data from the file in a tabular format.
+    }
+}
