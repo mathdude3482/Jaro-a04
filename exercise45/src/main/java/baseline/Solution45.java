eb1a1cf7d0d583848accf9d1affdbcc71173fba3
mathdude3482 <orajsamej@gmail.com>
2021-10-16 13:06:32 -0400

    First design of the solution along with a PlantUML diagram.

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,29 @@
+package baseline;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 James Jaro
+ */
+//Given an input file named exercise45_input.txt,
+// read the file and look for all occurrences of the word utilize. Replace each occurrence with use.
+// Write the modified file to a new file.
+public class Solution45 {
+    public static void main(String[] args) {
+        Solution45 myApp = new Solution45();
+        Scanner filePath = null;
+        //pass in the path of the input file to a scanner.
+        //call a method called modifyFile that will modify the input file.
+        //ask the user for the name of the output file.
+        //create the output file.
+        //write the output to the output file.
+    }
+    public void modifyFile(Scanner filePath, String oldString, String newString){
+        //take in three parameters: the path of the file, the old string that will be replaced,
+        //and the new string that will replace the old string.
+        //go through the file and read each line.
+        //for each line, if there is an instance of oldString, replace it with newString.
+        //write the new output to a new file.
+    }
+}
