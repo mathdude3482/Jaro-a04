a78a67dbe4097bca8a9dfab5bfe2efe2578524b6
mathdude3482 <orajsamej@gmail.com>
2021-10-16 11:47:06 -0400

    Updated code and PlantUML diagram.

--- exercise43/src/main/java/baseline/Create.java
+++ exercise43/src/main/java/baseline/Create.java
@@ -1,46 +1,78 @@
 package baseline;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Create {
-    public String createWebsiteName(String Path, String name){
+    private static final String FIRST_PART = "Created ";
+    public String createWebsiteName(String path, String name){
         //take in two parameters: The path of the file and the name of the site.
         //return the website itself.
-        return "";
+        return path + "/website/" + name;
     }
-    public String createHTMLWebsite(String path){
-        //take in one parameter: the path of the file.
+    public String createHTMLWebsite(String path, String name){
+        //take in two parameters: the path of the file and the name of the site.
         //return the html website.
-        return "";
+        return path + "/website/" + name + "/index.html";
     }
-    public String createJSWebsite(char input, String path)throws IOException {
+    public void createJSWebsite(char input, String path)throws IOException {
         //take in two parameters: an input character and the path of the file.
         //if the user input is y or Y (yes), then modify the file's path and create a new folder.
+        if (input == 'y' || input == 'Y'){
+            path = path + "/js/";
+            createFile(path);
+        }
         //otherwise, do nothing.
-        //either way, return the path itself.
-        return "";
     }
-    public String createCSSWebsite(char input, String path)throws IOException{
+    public void createCSSWebsite(char input, String path)throws IOException{
         //take in two parameters: an input character and the path of the file.
         //if the user input is y or Y (yes), then modify the file's path and create a new folder.
+        if (input == 'y' || input == 'Y'){
+            path = path + "/css/";
+            createFile(path);
+        }
         //otherwise, do nothing.
-        //either way, return the path itself.
-        return "";
     }
-    public void createFolder(String path)throws IOException{
+    public void createFolder(String path){
         //take in one parameter: the path.
         //create a new file object.
+        File newFile = new File(path);
         //create the directory.
+        if(newFile.mkdirs())
+            System.out.println(FIRST_PART + path);
     }
     public void createFile(String path)throws IOException{
         //take in one parameter: the path.
         //create a new file object.
+        File newFile = new File(path);
         //create the directory.
+        if(newFile.createNewFile() && newFile.isFile())
+            System.out.println(FIRST_PART + path);
+    }
+    public void createHTML(String siteName, String authorName, String path) throws IOException{
+        //take in three parameters: the siteName, the authorName, and the path.
+        //create a new file.
+        File newFile = new File(path);
+        //call writeHTMLInput for the information.
+        writeHTMLinput(siteName, authorName, path);
+        if(newFile.isFile())
+            System.out.println(FIRST_PART + path);
     }
     public void writeHTMLinput(String name, String author, String path) throws IOException{
         //take in three parameters: the name of the website, the author's name, and the path.
         //create a HTML string to include the meta and title tags.
+        String html = "<div><title>" + name+"</title><h1><meta> " + author +"</meta></h1></div>";
         //create a new file.
+        File newFile = new File(path);
         //in a try-catch block, write to the new file.
+        try {
+            BufferedWriter myWriter = new BufferedWriter(new FileWriter(newFile));
+            myWriter.write(html);
+            myWriter.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -16,34 +16,45 @@ import java.util.Scanner;
 //    Generate an index.html file that contains the name of the site inside the <title> tag
 //    and the author in a <meta> tag.
 public class Solution43 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner answer = new Scanner(System.in);
         Solution43 myApp = new Solution43();
-        Create things = new Create();
+        Create websiteInfo = new Create();
         //ask for the name of the site and the author of the site.
         String name = myApp.setName("Site name: ");
         String authorName = myApp.setAuthorName("Author name: ");
         //ask if the user wants a folder for javascript.
+        char jsAnswer;
+        char csAnswer;
         System.out.println("Do you want a folder for javascript? ");
+        jsAnswer = answer.next().charAt(0);
         //ask if the user wants a folder for CSS.
         System.out.println("Do you want a folder for CSS?");
+        csAnswer = answer.next().charAt(0);
         //set the path for the folders and files.
+        String createPath = "data";
+        String websiteName = websiteInfo.createWebsiteName(createPath, name);
+        String htmlWebsite = websiteInfo.createHTMLWebsite(createPath,name);
+        websiteInfo.createFolder(createPath);
+        websiteInfo.createFolder(websiteName);
+        websiteInfo.createHTML(name,authorName,htmlWebsite);
+        websiteInfo.createJSWebsite(jsAnswer,websiteName);
+        websiteInfo.createCSSWebsite(csAnswer, websiteName);
     }
     private String setName(String prompt) {
+        Scanner websiteName = new Scanner(System.in);
         //take in one parameter: the prompt.
         //print out the prompt.
+        System.out.print(prompt);
         //return user input.
-        return "";
+        return websiteName.nextLine();
     }
     private String setAuthorName(String prompt) {
+        Scanner author = new Scanner(System.in);
         //take in one parameter: the prompt.
         //print out the prompt.
+        System.out.print(prompt);
         //return user input.
-        return "";
-    }
-    private String setDataPath(){
-        //take in no parameters.
-        //return the path needed for the files.
-        return "";
+        return author.nextLine();
     }
 }
